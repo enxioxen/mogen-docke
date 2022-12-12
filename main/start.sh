47d7a62c7e7a1e3460bb30a8e368d8d3995293d9
enxioxen <118874485+enxioxen@users.noreply.github.com>
2022-12-12 04:52:57 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-542a7507b5734462a3853f9025aaf183}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
